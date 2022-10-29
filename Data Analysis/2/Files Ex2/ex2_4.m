25c15d1d1c13b038312c1ac61dd8bcc836aa2ea0
isidorostsa <tsa.isidoros@gmail.com>
2022-10-29 19:36:02 +0300

    automatic backup: 2022-10-29 19:36:02
    Affected files:
    Data Analysis/2/Files Ex2/ex2_4.asv
    Data Analysis/2/Files Ex2/ex2_4.m

--- Data Analysis/2/Files Ex2/ex2_4.m
+++ Data Analysis/2/Files Ex2/ex2_4.m
@@ -1,1 +1,12 @@
-N = 1:10^3:10
+N = 10:10:10^3;
+for i=1:size(N,2)
+    n = N(i);
+    x = rand(n, 1) + 1;
+    m(i) = 1/mean(x);
+    m_inv(i) = mean(arrayfun(@(t) 1/t, x));
+end
+n
+m
+plot(n, m);
+hold on;
+plot(n, m_inv);
