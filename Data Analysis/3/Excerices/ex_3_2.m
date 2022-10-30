edf8d2aa07ac76dd2cb1e17acf0318aa4c95fdcd
isidorostsa <tsa.isidoros@gmail.com>
2022-10-31 00:11:29 +0200

    automatic backup: 2022-10-31 00:11:29
    Affected files:
    Data Analysis/3/Excerices/ex_3_2.m

--- Data Analysis/3/Excerices/ex_3_2.m
+++ Data Analysis/3/Excerices/ex_3_2.m
@@ -1,18 +1,20 @@
+%1.
 % log L = n log l - l Sum x_i 
 % d log L / d l = n / l - Sum x_i = 0 
-% l = 1/mu
+% l = 1/x_bar
 
-
+%2.
 function mu_lambda_avg = ex_3_2 ()
     M = 5000;
     n = 7;
     lambda = 4;
    
+    % M columns of n rows
     S = exprnd(lambda, n, M);
 
-    mu_lambda = arrayfun(@(x) n/sum(x), S(,:));
-    
+    % sum(S) returns vector of the sums of columns
     mu_lambda = arrayfun(@(x) n/x, sum(S));
+
     mu_lambda_avg = sum(mu_lambda)/M
     hist(mu_lambda, 500);
 end
