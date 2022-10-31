4230976090cf84aba9d18e51e8b48d733ccf5ab7
isidorostsa <tsa.isidoros@gmail.com>
2022-10-31 20:58:58 +0200

    automatic backup: 2022-10-31 20:58:58
    Affected files:
    Data Analysis/3/Excerices/ex_3_3.m

--- Data Analysis/3/Excerices/ex_3_3.m
+++ Data Analysis/3/Excerices/ex_3_3.m
@@ -1,6 +1,15 @@
 function ex_3_3()
     n = 5;
-    M = 1000;
+    M = 10;
     
     lambda = 1/15;
     
+    s = exprnd(lambda, n, M);
+    
+    cnt = 0;
+    for sample=1:M
+        s(:,sample)
+        cnt = cnt + ttest(s(:,sample), lambda);
+    end
+    cnt/M
+end
