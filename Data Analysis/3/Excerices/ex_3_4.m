d702fa0c29d635ddded4da056b5c90c687acce00
isidorostsa <tsa.isidoros@gmail.com>
2022-11-01 10:18:22 +0200

    automatic backup: 2022-11-01 10:18:22
    Affected files:
    Data Analysis/3/Excerices/ex_3_4.m

--- Data Analysis/3/Excerices/ex_3_4.m
+++ Data Analysis/3/Excerices/ex_3_4.m
@@ -1,7 +1,19 @@
 function ex_3_4()
     s = [41 48 50 52 46 50 50 52 47 50 50 53 47 50 50 53 48 50 52 53 50 50 52 53 50 50 53 53 50 50 53 53 50 52 53 54 50 52 53 54 50 53 53 55 50 55 57 68];
     disp("95% interval");
+    [H p ci] = vartest(s, 5);
+    if(H)
+        disp("variance of 5kv is within the interval")
+    else
+        disp("variance of 5kv is not within the interval")
+    end
     
-    
+    disp("95% interval of mean");
+    [H p ci] = ttest(s, 52);
+    if(H)
+        disp("mean of 52kv is within the interval")
+    else
+        disp("mean of 52kv is not within the interval")
+    end
     
 end
