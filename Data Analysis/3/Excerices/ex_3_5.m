a3b99e175184a420927d535f969a73932e22c7b0
isidorostsa <tsa.isidoros@gmail.com>
2022-11-06 21:27:29 +0200

    automatic backup: 2022-11-06 21:27:29
    Affected files:
    Data Analysis/3/Excerices/ex_3_5.asv
    Data Analysis/3/Excerices/ex_3_5.m

--- Data Analysis/3/Excerices/ex_3_5.m
+++ Data Analysis/3/Excerices/ex_3_5.m
@@ -6,10 +6,11 @@ function ex_3_5()
     d89 = table2array(table(:,2));
     w06 = table2array(table(:,3));
 
-    n = size(w1989, 1);
+    n = size(w89, 1);
     
     % var normalizes with n-1 by default, so it gives s
 
-    sigma_sq_w89 = var(w89);
+    sigma_sq_w89 = var(w89)
 
-    
+    lo = (n-1)*sigma_sq_w89/(chi2inv(0.975, n-1))
+    hi = (n-1)*sigma_sq_w89/(chi2inv(0.025, n-1))
