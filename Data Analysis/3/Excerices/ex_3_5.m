3ae457e6b0a2b7613e9a9550c23e15cbde152c69
isidorostsa <tsa.isidoros@gmail.com>
2022-11-06 21:02:29 +0200

    automatic backup: 2022-11-06 21:02:29
    Affected files:
    Data Analysis/3/Excerices/ex_3_5.m

--- Data Analysis/3/Excerices/ex_3_5.m
+++ Data Analysis/3/Excerices/ex_3_5.m
@@ -1,9 +1,16 @@
 function ex_3_5() 
     table = readtable('eruption.dat')
 
-    w1989 = table2array(table(:,1));
-    d1989 = table2array(table(:,2));
-    w2006 = table2array(table(:,3));
+    % waiting and duration 
+    w89 = table2array(table(:,1));
+    d89 = table2array(table(:,2));
+    w06 = table2array(table(:,3));
 
-    n = size(w1989, 2)
+    n = size(w1989, 1);
+    
+    % var normalizes with n-1 by default, so it gives s
+
+    sigma_sq_w89 = var(w89);
+
+    
     
