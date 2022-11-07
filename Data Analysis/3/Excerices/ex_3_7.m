693fe15feab05b9c472db8dcc9cf080ece616c47
isidorostsa <tsa.isidoros@gmail.com>
2022-11-07 18:45:57 +0200

    automatic backup: 2022-11-07 18:45:57
    Affected files:
    Data Analysis/3/Excerices/ex_3_7.m

--- Data Analysis/3/Excerices/ex_3_7.m
+++ Data Analysis/3/Excerices/ex_3_7.m
@@ -3,14 +3,18 @@ M = 100;
 n = 10;
 B = 1000;
 
+X = normnd(0, 1, n, M);
 
-X = norrnd(0, 1, n, M);
-
-bs_mu = zeros(n, M);
-bs_var = zeros(n, M);
+bs_mu = zeros(B, M);
+bs_var = zeros(B, M);
 
 for m=1:M
+    
+
+
+    % bootstrap method
     for b=1:B
         sample = randsample(X(:,m), n, true);
-        bs_mu(B) = mean(sample)
-    
+        bs_mu(b, m) = mean(sample)
+    end
+end 
