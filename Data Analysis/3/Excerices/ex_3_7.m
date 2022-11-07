0561adcae8916a1a4816be2cac03ab5da9c7bbb5
isidorostsa <tsa.isidoros@gmail.com>
2022-11-07 18:50:57 +0200

    automatic backup: 2022-11-07 18:50:57
    Affected files:
    Data Analysis/3/Excerices/ex_3_7.asv
    Data Analysis/3/Excerices/ex_3_7.m

--- Data Analysis/3/Excerices/ex_3_7.m
+++ Data Analysis/3/Excerices/ex_3_7.m
@@ -6,11 +6,17 @@ B = 1000;
 X = normnd(0, 1, n, M);
 
 bs_mu = zeros(B, M);
+conf_ints = zeros(2, M);
+
 bs_var = zeros(B, M);
 
 for m=1:M
+    [h p ci] = ttest(X(:,m));
+    lo = ci(1);
+    hi = ci(2);
     
-
+    conf_ints(1, m) = lo;
+    conf_ints(2, m) = hi;
 
     % bootstrap method
     for b=1:B
