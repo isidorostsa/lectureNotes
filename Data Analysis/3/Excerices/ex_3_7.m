ed4781a307b8e4b68021caa37a7cde64d65f8531
isidorostsa <tsa.isidoros@gmail.com>
2022-11-07 18:55:57 +0200

    automatic backup: 2022-11-07 18:55:57
    Affected files:
    Data Analysis/3/Excerices/ex_3_7.m

--- Data Analysis/3/Excerices/ex_3_7.m
+++ Data Analysis/3/Excerices/ex_3_7.m
@@ -2,25 +2,32 @@ function ex_3_7()
 M = 100;
 n = 10;
 B = 1000;
+a = 0.05;
 
 X = normnd(0, 1, n, M);
 
 bs_mu = zeros(B, M);
-conf_ints = zeros(2, M);
+ci_par = zeros(2, M);
+ci_bs = zeros(2, M);
 
 bs_var = zeros(B, M);
 
 for m=1:M
     [h p ci] = ttest(X(:,m));
-    lo = ci(1);
-    hi = ci(2);
+    lo_par = ci(1);
+    hi_par = ci(2);
     
-    conf_ints(1, m) = lo;
-    conf_ints(2, m) = hi;
+    ci_par(1, m) = lo_par;
+    ci_par(2, m) = hi_par;
 
     % bootstrap method
     for b=1:B
         sample = randsample(X(:,m), n, true);
-        bs_mu(b, m) = mean(sample)
+        bs_mu(b, m) = mean(sample);
     end
+    
+    lo_bs = sort(bs_mu)()
+
+end 
+
 end 
