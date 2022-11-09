03cfce9246d524ab6619eb651d4cc8c036ed4b74
isidorostsa <tsa.isidoros@gmail.com>
2022-11-09 22:32:13 +0200

    no msg

--- Optimization Techniques/Project/ex_1_1.m
+++ Optimization Techniques/Project/ex_1_1.m
@@ -1,10 +1,12 @@
-function res = ex_1_1(func, lower_bound, upper_bound, epsilon, limit_width, limit_iterations)
+function [sol lb_list ub_list] = ex_1_1(func, lower_bound, upper_bound, epsilon, limit_width, limit_iterations)
     x1 = 0;
     x2 = 0;
 
     iter = 1;
 
     while upper_bound - lower_bound > limit_width && iter < limit_iterations
+        lb_list(iter) = lower_bound;
+        ub_list(iter) = upper_bound;
 
         x1 = (upper_bound + lower_bound) / 2 - epsilon;
         x2 = (upper_bound + lower_bound) / 2 + epsilon;
@@ -20,5 +22,5 @@ function res = ex_1_1(func, lower_bound, upper_bound, epsilon, limit_width, limi
         iter = iter + 1;
     end
 
-    res = (upper_bound + lower_bound)/2;
+    sol = (upper_bound + lower_bound)/2;
 end
