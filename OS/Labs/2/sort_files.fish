377a739ba1eef3bc17cc3b4aa0ecce6aa63ae33b
isidorostsa <tsa.isidoros@gmail.com>
2022-11-14 12:58:26 +0200

    automatic backup: 2022-11-14 12:58:26
    Affected files:
    OS/Labs/2/sort_files.fish

--- OS/Labs/2/sort_files.fish
+++ OS/Labs/2/sort_files.fish
@@ -3,11 +3,15 @@ function sort_files
     #mkdir -p $argv[1]/bytes $argv[1]/kilobytes $argv[1]/megabytes $argv[1]/gigabytes 1>dev/null
     string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} echo {}
     # byte sized
-    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -print0 
+    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -size -1024c -print0  | \
+    xargs -0 -I {} cp {} $argv[1]/bytes 1>dev/null 
     # kilobyte sized
-    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -print0 
+    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -print0 -size +1023c -size -1048576c | \
+    xargs -0 -I {} cp {} $argv[1]/kilobytes 1>dev/null
     # megabyte sized
-    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -print0 
+    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -print0 -size +1048575c -size -1073741824c | \
+    xargs -0 -I {} cp {} $argv[1]/megabytes 1>dev/null
     # gigabyte sized
-    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -print0 
+    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -print0 -size +1073741823c | \
+    xargs -0 -I {} cp {} $argv[1]/gigabytes 1>dev/null
 end
