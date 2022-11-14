dddb14b61e4f9390f126ce4bb75f468f7c85fd05
isidorostsa <tsa.isidoros@gmail.com>
2022-11-14 12:38:26 +0200

    automatic backup: 2022-11-14 12:38:26
    Affected files:
    .obsidian/workspace.json
    OS/Labs/2/sort_files.fish

--- OS/Labs/2/sort_files.fish
+++ OS/Labs/2/sort_files.fish
@@ -1,4 +1,4 @@
 function sort_files
     #mkdir -p $argv[1]/bytes $argv[1]/kilobytes $argv[1]/megabytes $argv[1]/gigabytes 1>dev/null
-    find . -regex '.*/$argv[2][^/]*\.$argv[1]'
+    string join '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} echo {}
 end
