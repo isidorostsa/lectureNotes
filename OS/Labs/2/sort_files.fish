46c43ada8b934102eca4a8620b1d8420cd6b0a52
isidorostsa <tsa.isidoros@gmail.com>
2022-11-14 12:43:26 +0200

    automatic backup: 2022-11-14 12:43:26
    Affected files:
    OS/Labs/2/sort_files.fish

--- OS/Labs/2/sort_files.fish
+++ OS/Labs/2/sort_files.fish
@@ -1,4 +1,5 @@
 function sort_files
-    #mkdir -p $argv[1]/bytes $argv[1]/kilobytes $argv[1]/megabytes $argv[1]/gigabytes 1>dev/null
-    string join '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} echo {}
+    string join ".*/" $argv[2] "[^/]*\." $argv[1] | xargs -I {} echo {}
 end
+
+#mkdir -p $argv[1]/bytes $argv[1]/kilobytes $argv[1]/megabytes $argv[1]/gigabytes 1>dev/null
