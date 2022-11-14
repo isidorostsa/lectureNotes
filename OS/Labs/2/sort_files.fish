97c93a95cd9ee83d5d0478da3cdc2abac853a095
isidorostsa <tsa.isidoros@gmail.com>
2022-11-14 12:53:26 +0200

    automatic backup: 2022-11-14 12:53:26
    Affected files:
    OS/Labs/2/sort_files.fish

--- OS/Labs/2/sort_files.fish
+++ OS/Labs/2/sort_files.fish
@@ -1,5 +1,13 @@
 function sort_files
+    # create dirs
+    #mkdir -p $argv[1]/bytes $argv[1]/kilobytes $argv[1]/megabytes $argv[1]/gigabytes 1>dev/null
     string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} echo {}
-    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex -{} -print0
+    # byte sized
+    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -print0 
+    # kilobyte sized
+    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -print0 
+    # megabyte sized
+    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -print0 
+    # gigabyte sized
+    string join '' '.*/' $argv[2] '[^/]*\.' $argv[1] | xargs -I {} find . -regex {} -print0 
 end
-#mkdir -p $argv[1]/bytes $argv[1]/kilobytes $argv[1]/megabytes $argv[1]/gigabytes 1>dev/null
