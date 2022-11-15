cf61db34ffb8f9547ca161d7f643c4d978b516f9
isidorostsa <tsa.isidoros@gmail.com>
2022-11-15 19:55:25 +0200

    automatic backup: 2022-11-15 19:55:24
    Affected files:
    Parallel & Distributed Systems/Project/test.jl

--- Parallel & Distributed Systems/Project/test.jl
+++ Parallel & Distributed Systems/Project/test.jl
@@ -17,14 +17,7 @@ graph = DiGraph(graphMatrix);
 gplot(graph, nodelabel=1:nv(graph), nodefillc=:"lightblue", nodesize=0.1)
 
 # set of the neighboors of node 1
-neighbors(graph, 261)
+neighbors(graph, 1)
 
-# empty array of graphs
-graphs = Array{Graph, 1}();
-
-# populate it's first entry with the connected components of node 1
-graphs[1] = neighboors(graph, 1);
-
-# plot the first graph
-
-gplot(graphs[1], nodelabel=1:nv(graphs[1]), nodefillc=:"lightblue", nodesize=0.1)
+# graph with the neighboors of node 1
+gplot(graph, nodelabel=1:nv(graph), nodefillc=:"lightblue", nodesize=0.1, edgelabel=1:ne(graph), edgestrokec=:"lightblue", edgestrokewidth=0.1, highlight=neighbors(graph, 1))
