001d420f85a2f6f2b7316c32bb19ca9a61c525e2
isidorostsa <tsa.isidoros@gmail.com>
2022-11-16 14:34:24 +0200

    test

--- Parallel & Distributed Systems/Project/test.jl
+++ Parallel & Distributed Systems/Project/test.jl
@@ -16,7 +16,43 @@ graph = DiGraph(graphMatrix);
 # plot the graph
 gplot(graph, nodelabel=1:nv(graph), nodefillc=:"lightblue", nodesize=0.1)
 
-# set of the neighboors of node 1
-neighbors(graph, 1)
+# create the following directed graph:
+# 1 -> 2 -> 3 -> 1 and 4 -> 5 -> 6 -> 4 and 1 -> 4
+g = DiGraph(6)
+add_edge!(g, 1, 2)
+add_edge!(g, 2, 3)
+add_edge!(g, 3, 1)
+add_edge!(g, 4, 5)
+add_edge!(g, 5, 6)
+add_edge!(g, 6, 4)
+add_edge!(g, 1, 4)
 
-# undirected graph containing the same edges as graph
+gplot(g, nodelabel=1:nv(g), nodefillc=:"lightblue", nodesize=0.1)
+
+function trimGraphOnce!(digraph)
+    # iterate over nodes
+    madeChange = false
+    # array of nodes to be removed
+    nodesToRemove = []
+    for node in vertices(digraph)
+        # if the node has no incoming edges
+        if indegree(digraph, node) == 0 || outdegree(digraph, node) == 0
+            # add the node to the array of nodes to be removed
+            push!(nodesToRemove, node)
+            madeChange = true
+        end
+    end
+    # remove the nodes
+    for node in nodesToRemove
+        rem_vertex!(digraph, node)
+    end
+    return madeChange
+end
+
+trimGraphOnce!(g)
+
+while trimGraphOnce!(g)
+end
+
+# plot g
+gplot(g, nodelabel=1:nv(g), nodefillc=:"lightblue", nodesize=0.1)
