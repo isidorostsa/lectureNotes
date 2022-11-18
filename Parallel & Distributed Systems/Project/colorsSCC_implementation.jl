1890ec04a6c8cc44b523a6734163075c25bc063c
isidorostsa <tsa.isidoros@gmail.com>
2022-11-18 13:25:45 +0200

    improve jl

--- Parallel & Distributed Systems/Project/colorsSCC_implementation.jl
+++ Parallel & Distributed Systems/Project/colorsSCC_implementation.jl
@@ -19,6 +19,10 @@ add_edge!(g, 6, 4)
 # and its plot
 gplot(g, nodelabel=1:nv(g), nodefillc=:"lightblue", nodesize=0.1)
 
+# adjacency matrix
+A = adjacency_matrix(g)
+
+
 # returns the nodes reachable from the given node
 function bfs(graph, source)
     # int64 vector of visited nodesSCV_c
@@ -133,13 +137,19 @@ scc = colorSCC(g)
 am = adjacency_matrix(g)
 
 # bfs as sparse matrix multiplication with a vector
-function bfs_matrix(adjecency_matrix, source)
+function bfs_matrix2(adjecency_matrix, source, allowed_vertices)
 
-    current = source
+    source = Vector{Bool}(source)
+    current = copy(source)
 
     while true
-        current = adjecency_matrix' * current
-        current = min.((source.|current), 1)
+        # select the cols of am with indeces in current
+        cols = adjecency_matrix'[:, Vector{Bool}(current)]
+
+        current = source
+        for i=1:size(cols, 2)
+            current = (current .| (cols[:, i] .& allowed_vertices))
+        end
 
         if current == source
             break
@@ -147,36 +157,140 @@ function bfs_matrix(adjecency_matrix, source)
 
         source = current
     end
-    return current
+
+    return Vector{Bool}(current)
 end
 
-function trimGraph_matrix!(adjacency_matrix)
+function bfs_matrix(adjecency_matrix, source, allowed_vertices)
+
+    source = Vector{Bool}(source)
+    current = copy(source)
+    while true
+        # select the cols of am with indeces in current
+        current = (adjecency_matrix' * source) .| current
+
+        current = Vector{Bool}(min.(current, 1))
+
+        current = .&(current, allowed_vertices)
+
+        if current == source
+            break
+        end
+
+        source = current
+    end
+
+    return Vector{Bool}(current)
+end
+
+bfs_matrix(A, [1, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0])
+
+function trimedVertices_matrix(am)
+
+    adjacency_matrix = copy(am)
+
+    trimedVertices = []
     made_change = true
     while made_change
         made_change = false
         for i in 1:size(adjacency_matrix, 1)
+            if i in trimedVertices
+                continue
+            end
             if sum(adjacency_matrix[i, :]) == 0 || sum(adjacency_matrix[:, i]) == 0
-                adjacency_matrix[i, :] = 0
-                adjacency_matrix[:, i] = 0
+                adjacency_matrix[i, :] .= 0
+                adjacency_matrix[:, i] .= 0
                 made_change = true
+                push!(trimedVertices, i)
             end
         end
     end
+    return trimedVertices
 end
 
 function colorSCC_matrix(adjecency_matrix)
-    am = copy(adjecency_matrix)
+    M = copy(adjecency_matrix)
 
-    trimGraph_matrix!(am)
+    n = M.n
 
-    n = am.n
+    SCC = []
+    #bitmap of vetrices left:
+    vleft = ones(Bool, n)
 
-    vleft = [1:n...]
+    for v in trimedVertices_matrix(M)
+        vleft[v] = false
+        push!(SCC, [v])
+    end
 
-    colors = zeros(Int64, size(am, 1))
+    colors = zeros(Int64, size(M, 1))
 
-    while length(vleft) > 0
+    in_nei_vec = []
+    for i_m in 1:M.m
+        push!(in_nei_vec, findall(x -> x != 0, M[:, i_m]))
+    end
+
+    out_nei_vec = []
+    for i_m in 1:M.m
+        push!(out_nei_vec, findall(x -> x != 0, M[i_m, :]))
+    end
 
 
+    # to find outneighbors of i:
+    # out_nei_vec[i]  in_nei_vec[i]
 
+    while reduce(|, vleft)
+        for i in 1:n
+            if vleft[i]
+                colors[i] = i
+            else
+                colors[i] = 0
+            end
+        end
+
+        made_change = true
+        while made_change
+            made_change = false
+            for i in 1:n
+                if vleft[i]
+                    for j in out_nei_vec[i]
+                        if vleft[j]
+                            if colors[j] < colors[i]
+                                colors[j] = colors[i]
+                                made_change = true
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
+        for color in unique(colors)
+            if color == 0
+                continue
+            end
+
+            Vc = [colors[i] == color for i in 1:n]
+
+            source = zeros(Bool, n)
+            source[color] = true
+
+            vertices_in_rev_bfs = bfs_matrix(M', source, Vc)
+
+            push!(SCC, [i for i in 1:n if vertices_in_rev_bfs[i]])
+
+            vleft = vleft .& .!vertices_in_rev_bfs
+        end
+    end
+    return SCC
 end
+
+colorSCC_matrix(A)
+# Load the matrix
+graphMatrix = mmread("matrices/foldoc/foldoc.mtx");
+
+# convert graphMatrix to a sparse matrix object
+graphMatrix = sparse(graphMatrix);
+
+colorSCC_matrix(graphMatrix)
+
+# DO TRIM AFTER NEIGHBORS ARE FOUND 
