01dc1dd248d8b403612498e6b8e524e192b1281b
isidorostsa <tsa.isidoros@gmail.com>
2022-11-20 20:56:40 +0200

    dont create folders you won't fill

--- OS/Labs/2/main.fish
+++ OS/Labs/2/main.fish
@@ -1,16 +1,29 @@
 function sort_files
-    # create dirs
-    mkdir -p $argv[2]/bytes $argv[2]/kilobytes $argv[2]/megabytes $argv[2]/gigabytes
     # byte sized
-    find . -name "$argv[2]*.$argv[1]" -size -1024c -print0 |\
-    xargs -0 -I {} cp {} $argv[2]/bytes
+    if find . -name "$argv[2]*.$argv[1]" -size -1024c -print0 | tee bytes.my_special_file | grep . > /dev/null
+        mkdir -p $argv[2]/bytes
+        cat bytes.my_special_file | xargs -0 -I {} cp {} $argv[2]/bytes 1>/dev/null
+    end
+    rm bytes.my_special_file
+
     # kilobyte sized
-    find . -name "$argv[2]*.$argv[1]"  -size +1023c -size -1048576c  -print0 |\
-     xargs -0 -I {} cp {} $argv[2]/kilobytes
+    if find . -name "$argv[2]*.$argv[1]" -size +1023c -size -1048576c -print0 | tee kilobytes.my_special_file | grep . > /dev/null
+        mkdir -p $argv[2]/kilobytes
+        cat kilobytes.my_special_file | xargs -0 -I {} cp {} $argv[2]/kilobytes 1>/dev/null
+    end
+    rm kilobytes.my_special_file
+
     # megabyte sized
-    find . -name "$argv[2]*.$argv[1]"  -size +1048575c -size -1073741824c   -print0 |\
-    xargs -0 -I {} cp {} $argv[2]/megabytes
-    # gigabyte sized
-    find . -name "$argv[2]*.$argv[1]"  -size +1073741823c  -print0 |\
-    xargs -0 -I {} cp {} $argv[2]/gigabytes
+    if find . -name "$argv[2]*.$argv[1]" -size +1048575c -size -1073741824c -print0 | tee megabytes.my_special_file | grep . > /dev/null
+        mkdir -p $argv[2]/megabytes
+        cat megabytes.my_special_file | xargs -0 -I {} cp {} $argv[2]/megabytes 1>/dev/null
+    end
+    rm megabytes.my_special_file
+
+    # gigabyte and bigger sized
+    if find . -name "$argv[2]*.$argv[1]" -size +1073741823c -print0 | tee gigabytes.my_special_file | grep . > /dev/null
+        mkdir -p $argv[2]/gigabytes
+        cat gigabytes.my_special_file | xargs -0 -I {} cp {} $argv[2]/gigabytes 1>/dev/null
+    end
+    rm gigabytes.my_special_file
 end
--- Parallel & Distributed Systems/Project/CppScc/hello.cpp
+++ Parallel & Distributed Systems/Project/CppScc/hello.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
-using namespace std;
+#include <fstream>
+#include <algorithm>
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    // Open the file
+    std::ifstream fin("../matrices/foldoc/foldoc.mtx");
 
-    for (const string& word : msg)
+    int M, N, L;
+    // M - number of rows
+    // N - number of columns
+    // L - number of non-zero elements
+
+    // Read the first line
+    while(fin.peek() == '%') fin.ignore(2048, '\n');
+
+    // The first line contains the number of rows, columns and non-zero elements
+    fin >> M >> N >> L;
+
+    double* matrix;
+    matrix = new double[M*N];
+
+    std::fill(matrix, matrix + M*N, 0);
+
+    for(int l = 0; l < L; l++)
     {
-        cout << word << " ";
+        int i, j;
+        double value;
+
+        fin >> i >> j >> value;
+
+        std::cout << i << " " << j << " " << value << std::endl;
+        std::cin >> value;
+
+        matrix[(i-1)*N + (j-1)] = value;
     }
-    cout << endl;
 }
