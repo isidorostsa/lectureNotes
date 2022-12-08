53067ec9527298eec8707d98e2b0ced2d949726f
isidorostsa <tsa.isidoros@gmail.com>
2022-12-08 02:01:09 +0200

    automatic backup: 2022-12-08 02:01:09
    Affected files:
    Optimization Techniques/Project/2/Ex1/ex_2_1.m

--- Optimization Techniques/Project/2/Ex1/ex_2_1.m
+++ Optimization Techniques/Project/2/Ex1/ex_2_1.m
@@ -1,14 +1,12 @@
 %% Steepest Descent Method
-function [plist glist] = ex_2_1(func, max_iterations)
+function [plist glist] = ex_2_1(func, gradfunc, max_iterations)
     plist = [];
     glist = [];
     p = [0; 0];
     plist = [plist p];
-    glist = [glist g];
 
-    for i = 1:max_iterations
-        p = p - g;
-        plist = [plist p];
-        g = gradient(func, p);
-        glist = [glist g];
+    while max_iterations > 0
+        d = gradfunc(p);
+
+        
     end
