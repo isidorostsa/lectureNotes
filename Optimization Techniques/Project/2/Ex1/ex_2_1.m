4e482939435f7401ee146c3ec909723c04a85066
isidorostsa <tsa.isidoros@gmail.com>
2022-12-12 13:36:49 +0200

    numerize

--- Optimization Techniques/Project/2/Ex1/ex_2_1.m
+++ Optimization Techniques/Project/2/Ex1/ex_2_1.m
@@ -1,18 +1,15 @@
 %% Steepest Descent Method
-function finalValue = ex_2_1(func, gradfunc, max_iterations, p0, gamma_selection_method)
+function [finalPoint finalValue iterations] = ex_2_1(f_symbolic, x, y, max_iterations, p0, d_selection_method, gamma_selection_method)
+    [func gradfunc hessianfunc] = numerize(f_symbolic, x, y)
+
     plist = [];
     glist = [];
     p = p0;
     plist = [plist p];
 
-    function k = temp_func(gamma, p)
-        grad = gradfunc(p);
-        arg = p - gamma * grad;
-        k =  gradfunc(arg)*grad;
-    end
-    
-    grad = [1; 1];
-    while norm(grad) > 1e-6 && max_iterations > 0
+    grad_at_p = [1; 1];
+    iterations = 0;
+    while norm(gradfunc(p)) > 1e-3 && iterations < max_iterations
         % we want to find the step size that minimizes the function
         % f(p + gamma * grad(p))
         % we can do this by finding the zero of the derivative of f
@@ -21,26 +18,58 @@ function finalValue = ex_2_1(func, gradfunc, max_iterations, p0, gamma_selection
         % we can use the bisection method to find the zero of the
         % derivative
         % ezplot(@(gamma) temp_func(gamma, p), [0 20])
-        grad = gradfunc(p);
+        grad_at_p = gradfunc(p);
+
+        % steepest descent
+        if(d_selection_method == 1)
+            d = -grad_at_p;
+        % newton
+        elseif(d_selection_method == 2)
+            hessian_at_p = hessianfunc(p);
+            d = -inv(hessian_at_p)*grad_at_p;
+        % L-M
+        elseif(d_selection_method == 3)
+            hessian_at_p = hessianfunc(p);
+            u_search_interval = linspace(0, 60, 100);
+
+            matrix_to_be_pos_def = @(u) hessian_at_p + u*eye(2);
+
+            u = -100;
+
+            for i = 1:length(u_search_interval)
+                if (det(matrix_to_be_pos_def(u_search_interval(i))) > 0 && hessian_at_p(1, 1) + u_search_interval(i) > 0)
+                    u = u_search_interval(i);
+                    break;
+                end
+            end
+
+            if u == -100
+                disp('No u found');
+            end
+
+            d = inv(matrix_to_be_pos_def(u))*(-grad_at_p);
+        end
+
+        % constant
         if(gamma_selection_method == 1)
             gamma = 0.01;
+        % simple minimize
         elseif(gamma_selection_method == 2)
-            gamma = fminbnd(@(gamma) func(p - gamma*grad), 0, 3);
+            gamma = fminbnd(@(gamma) func(p + gamma*d), 0, 10);
+        %armijo
         elseif(gamma_selection_method == 3)
             a = 1e-3;
             b = 1e-1;
             s = 1e-1;
-            gamma_of_m = @(m) s*b^m;
-            d = -grad;
-            
+
             gamma = armijo(func, gradfunc(p), d, a, b, s, p);
         end
 
-        p = p - gamma*grad;
+        p = p + gamma*d;
         plist = [plist p];
 
         grad = gradfunc(p);
-        max_iterations = max_iterations - 1;
+        iterations = iterations + 1;
     end
     finalPoint = p;
     finalValue = func(p);
